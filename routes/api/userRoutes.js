21456342f1f19df9ac8c1c87ba8ddc38ef49bc2f
Jakob A <jdanders1999@gmail.com>
2023-03-12 16:54:55 -0400

    Added route to post and delete friends, as well as exports in userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,3 +9,9 @@ router.route('/:userId')
     .get(getSingleUser)
     .put(updateUser)
     .delete(deleteUser);
+
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
+module.exports = router;
