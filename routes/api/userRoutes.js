1980bc8965e86ac76ff4031b1d1e8d337c3b8380
Jakob A <jdanders1999@gmail.com>
2023-03-12 16:52:29 -0400

    Set up express and required connections for userRoutes.js, created get and post route for users

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend, removeFriend } = require('../../controllers/userController');
+
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
