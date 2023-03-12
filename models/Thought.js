b1d2c31c026d332010b06ec282252d1152c6a349
Jakob A <jdanders1999@gmail.com>
2023-03-12 15:14:17 -0400

    Added thoughtSchema to Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,30 @@
+const { Schema, model, Types } = require('mongoose');
+const dayjs = require('dayjs');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtTime => dayjs(createdAtTime).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
