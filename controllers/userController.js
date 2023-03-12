f50da3944f0eac9d4c79ddaf423d138d8d25b5c2
Jakob A <jdanders1999@gmail.com>
2023-03-12 16:18:35 -0400

    Added setup to get user or a single user by id

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    getUsers(req, res) {
+        User.find()
+            .select('-__v')
+            .then((data) => res.json(data))
+            .catch((err) => res.status(500).json(err));
+    },
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: 'Error - User not found' });
+                }
+                res.json(data);
+            })
+            .catch((err) => res.status(500).json(err));
+    },
+}
